03d377a16a3925997fce5fbd17dadd8a09b5263e
MaxProger338 <152500777+MaxProger338@users.noreply.github.com>
2023-12-01 12:37:44 +0700

    Add files via upload
    Signed-off-by: MaxProger338 <152500777+MaxProger338@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,19 @@
+function play() {
+    const audio1 = new Audio('o-privet.mp3');
+    audio1.play();
+}
+
+function play2() {
+    const audio2 = new Audio('povezlo-povezlo.mp3');
+    audio2.play();
+}
+
+function play3() {
+    const audio3 = new Audio('tutututu-mem-demotivator.mp3');
+    audio3.play();
+}
+
+function play4() {
+    const audio4 = new Audio('amogus.mp3');
+    audio4.play();
+}
