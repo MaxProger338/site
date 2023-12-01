6981364dcfd45c48c0edc843cd958a41582bbc20
MaxProger338 <152500777+MaxProger338@users.noreply.github.com>
2023-12-01 13:00:39 +0700

    Update script.js
    Signed-off-by: MaxProger338 <152500777+MaxProger338@users.noreply.github.com>

--- script.js
+++ script.js
@@ -17,3 +17,43 @@ function play4() {
     const audio4 = new Audio('amogus.mp3');
     audio4.play();
 }
+
+function play5() {
+    const audio1 = new Audio('ne-probil.mp3');
+    audio1.play();
+}
+
+function play6() {
+    const audio2 = new Audio('mem-okontsovka-filma-to-be-continued.mp3');
+    audio2.play();
+}
+
+function play7() {
+    const audio3 = new Audio('strashno-ochen-strashno.mp3');
+    audio3.play();
+}
+
+function play8() {
+    const audio4 = new Audio('kazahstan-ugrojaet-nam-bombordirovkoi.mp3');
+    audio4.play();
+}
+
+function play9() {
+    const audio1 = new Audio('bababooey-sound-effect.mp3');
+    audio1.play();
+}
+
+function play10() {
+    const audio2 = new Audio('rejisser-kiborg-ubiytsa.mp3');
+    audio2.play();
+}
+
+function play11() {
+    const audio3 = new Audio('brue.mp3');
+    audio3.play();
+}
+
+function play12() {
+    const audio4 = new Audio('luchshe-imet-druga.mp3');
+    audio4.play();
+}
